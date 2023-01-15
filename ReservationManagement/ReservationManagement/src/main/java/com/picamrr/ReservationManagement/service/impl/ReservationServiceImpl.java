a5cfb40922c8fba03fa0ab37e00f3b883a397d2d
Monica P <popamonica03@yahoo.com>
2023-01-15 15:20:03 +0200

    "send email"

--- ReservationManagement/ReservationManagement/src/main/java/com/picamrr/ReservationManagement/service/impl/ReservationServiceImpl.java
+++ ReservationManagement/ReservationManagement/src/main/java/com/picamrr/ReservationManagement/service/impl/ReservationServiceImpl.java
@@ -9,25 +9,42 @@ import com.picamrr.ReservationManagement.repository.RestaurantRepository;
 import com.picamrr.ReservationManagement.repository.UserRepository;
 import com.picamrr.ReservationManagement.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationServiceImpl implements ReservationService {
+    private static final String EMAIL_SUBJECT = " \uD83D\uDD14 Ready to have a fun time out? Your reservation is confirmed!";
+    private static final String EMAIL_BODY = "Hello, %s\n Thank you for choosing PICAMRR!\n\n Your reservation at %s on %s in the interval %s for %s people is confirmed. " +
+            "Free cancellation is available up to one day before your reservation date.\n\n" +
+            "Restaurant info:\n" +
+            "\uD83D\uDCCD  Address: %s\n" +
+            "☎️Phone number: %s\n\n" +
+            "Don't forget to rate the restaurant on our app! ⭐\n\n" +
+            "Have a great time!";
 
     private final ReservationRepository reservationRepository;
     private final UserRepository userRepository;
     private final RestaurantRepository restaurantRepository;
+    private final EmailSenderService emailSenderService;
 
     @Override
     public Reservation save(String userEmail, Long idRestaurant, Reservation reservation) {
         User user = userRepository.findByEmail(userEmail).get();
         Restaurant restaurant = validateRestaurant(idRestaurant);
         validateNoOfSeats(reservation, restaurant);
-        return reservationRepository.save(setUpReservation(reservation, restaurant, user));
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        var response = reservationRepository.save(setUpReservation(reservation, restaurant, user));
+        String message = String.format(EMAIL_BODY, user.getName(), restaurant.getName(), dateFormat.format(reservation.getDateOfReservation()),
+                reservation.getGap(), reservation.getNoOfSeats(), restaurant.getAddress(), restaurant.getPhoneNumber());
+        emailSenderService.sendEmail(userEmail, EMAIL_SUBJECT, message);
+        return response;
     }
 
     @Override
